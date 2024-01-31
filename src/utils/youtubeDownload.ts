11813ddbdf1e6d54df7bbdadec3ed19e4171df16
yoshi-non <himekawa.yoshino44@gmail.com>
2024-01-31 15:51:34 +0900

    大規模データでも保存できるようにする

--- src/utils/youtubeDownload.ts
+++ src/utils/youtubeDownload.ts
@@ -1,24 +1,40 @@
+'use client';
+
 import { getYoutubeBase64Data } from './getYoutubeBase64Data';
 
 export const youtubeDownload = async (
   youtubeId: string
 ) => {
-  const res = await getYoutubeBase64Data(youtubeId);
-  if (!res.data) return;
-  const filename = `${youtubeId}.mp4`;
-  const base64Data = res.data.replace(/^data:.*,/, '');
-  const buffer = Uint8Array.from(atob(base64Data), (c) =>
-    c.charCodeAt(0)
-  );
-  const blob = new Blob([buffer], {
-    type: 'application/octet-stream',
-  });
-  const objectURL = window.URL.createObjectURL(blob);
-  const link = document.createElement('a');
-  document.body.appendChild(link);
-  link.href = objectURL;
-  link.download = filename;
-  link.click();
-  document.body.removeChild(link);
-  window.URL.revokeObjectURL(objectURL);
+  try {
+    const res = await getYoutubeBase64Data(youtubeId);
+    if (!res.data) return;
+
+    const filename = `${youtubeId}.mp4`;
+    const base64Data = res.data.replace(/^data:.*,/, '');
+
+    // Base64データを直接デコード
+    const binaryData = atob(base64Data);
+
+    // ArrayBufferに変換
+    const arrayBuffer = new ArrayBuffer(binaryData.length);
+    const uint8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < binaryData.length; i++) {
+      uint8Array[i] = binaryData.charCodeAt(i);
+    }
+
+    // Blobを作成
+    const blob = new Blob([arrayBuffer], {
+      type: 'application/octet-stream',
+    });
+
+    // ダウンロード用のリンクを作成し、クリックしてダウンロード
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    document.body.removeChild(link);
+  } catch (error) {
+    console.log(error);
+  }
 };
