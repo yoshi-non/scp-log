417dad60ad025fbaa40435118c17f445b9418317
yoshi-non <himekawa.yoshino44@gmail.com>
2024-02-02 12:06:52 +0900

    netlifyで検証

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true,
     domains: ['i.ytimg.com', 'yt3.ggpht.com'],
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
