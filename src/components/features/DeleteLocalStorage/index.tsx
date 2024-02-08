9b5e5daa51e249cdb679bb170053c76387a9661c
yoshi-non <himekawa.yoshino44@gmail.com>
2024-02-08 15:36:53 +0900

    全データ削除処理追加

--- src/components/features/DeleteLocalStorage/index.tsx
+++ src/components/features/DeleteLocalStorage/index.tsx
@@ -9,8 +9,13 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
+import { localStorageKey } from '@/constants/localStorageKey';
+import { saveToLocalStorage } from '@/utils/storage';
 
 const DeleteLocalStorage = () => {
+  const deleteAllLocalStorageHandler = () => {
+    saveToLocalStorage(localStorageKey, []);
+  };
   return (
     <div className="mx-20">
       <p className="mt-5 text-xl font-bold">
@@ -23,12 +28,20 @@ const DeleteLocalStorage = () => {
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>
-              プレイリストの全データを削除しますか？
+              プレイリストの全データは削除されます。
+              <br />
+              よろしいですか？
             </AlertDialogTitle>
           </AlertDialogHeader>
           <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
+            <AlertDialogCancel>
+              キャンセル
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={deleteAllLocalStorageHandler}
+            >
+              削除
+            </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
