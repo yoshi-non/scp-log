830a1f0752c77f5e0ddbe16c764559eabf146600
yoshi-non <himekawa.yoshino44@gmail.com>
2024-02-14 21:50:49 +0900

    ファイル確認

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -3,7 +3,9 @@
 import { searchFiles } from '@/utils/existFile';
 
 const TestPage = () => {
-  console.log(searchFiles('/dict/base.dat.gz'));
+  console.log(searchFiles('./dict/base.dat.gz'));
+  console.log(searchFiles('/node_modules'));
+  console.log(searchFiles('./node_modules'));
   return <div className="container"></div>;
 };
 
